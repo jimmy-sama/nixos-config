d719c693a212d93a85d8e8dcac4f0f53403a0f9b
jimmy-sama <lungmuss@proton.me>
2023-10-12 19:58:58 +0200

    remove path

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -7,7 +7,6 @@
       ".config/i3/wallpaper.png".source = ../../wallpaper.png;
       ".config/i3/config".source = ./config;
       ".config/i3/i3blocks.conf".source = ./i3blocks.conf;
-      ".config/i3/keybindings".source = ./keybindings;
       ".config/i3/scripts" = {
           source = ./scripts;
           recursive = true;

ea0599d54b850e1275c62de7cb8cd6c858c72318
jimmy-sama <lungmuss@proton.me>
2023-10-12 18:18:53 +0200

    remove bat theme string

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -49,13 +49,6 @@
       enable = true;
       config = {
         pager = "less -FR";
-        theme = "Catppuccin-mocha";
-      };
-      themes = {
-        Catppuccin-mocha = builtins.readFile (pkgs.fetchurl {
-          url = "https://raw.githubusercontent.com/catppuccin/bat/main/Catppuccin-mocha.tmTheme";
-          hash = "sha256-qMQNJGZImmjrqzy7IiEkY5IhvPAMZpq0W6skLLsng/w=";
-        });
       };
     };
 

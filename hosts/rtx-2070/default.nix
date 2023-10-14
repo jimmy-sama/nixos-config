fa1429c2fa624302762f69cb26b2ee901cc98054
jimmy-sama <lungmuss@proton.me>
2023-10-14 21:30:35 +0200

    alter nvidia settings

--- hosts/rtx-2070/default.nix
+++ hosts/rtx-2070/default.nix
@@ -31,12 +31,21 @@
 
   # for Nvidia GPU
   services.xserver.videoDrivers = ["nvidia"];
-  hardware.opengl.enable = true;
-  hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-    modesetting.enable = true;
+  hardware = {
+      opengl = {
+          enable = true;
+          driSupport = true;
+          driSupport32Bit = true;
+      };
+      nvidia = {
+          modesetting.enable = true;
+          powerManagement.enable = false;
+          powerManagement.finegrained = false;
+          open = true;
+          nvidiaSettings = true;
+          package = config.boot.kernelPackages.nvidiaPackages.stable;
+      };
   };
-
   system.stateVersion = "23.11";
 }
 

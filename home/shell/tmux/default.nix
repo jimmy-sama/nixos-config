85c77ae3a4360e6450c63dde046156e3edc8c75d
jimmy-sama <lungmuss@proton.me>
2023-10-15 16:18:36 +0200

    tmux conf to nix conf

--- home/shell/tmux/default.nix
+++ home/shell/tmux/default.nix
@@ -1,13 +1,14 @@
 {
   programs.tmux = {
     enable = true;
-    plugins = [
+    plugins = with pkgs.tmuxPlugins [
             catppuccin
             sensible
         ];
-    tmuxConfig = ''
-        set -s default-terminal 'tmux-256color'
-
+    baseIndex = 1;
+    prefix = "C-a";
+    terminal = "tmux-256color"
+    extraConfig = ''
         set -g @catppuccin_window_right_separator "█ "
         set -g @catppuccin_window_number_position "right"
         set -g @catppuccin_window_middle_separator " | "
@@ -19,19 +20,6 @@
         set -g @catppuccin_date_time_text "%m-%d %H:%M"
         set -g @catppuccin_flavour 'macchiato'
 
-        unbind C-b
-        set-option -g prefix C-a
-        bind-key C-a send-prefix
-
-        is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
-        | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
-        bind-key -n C-h  if-shell  "$is_vim"  "send-keys C-h"  "select-pane -L"
-        bind-key -n C-j   if-shell  "$is_vim"  "send-keys C-j"   "select-pane -D"
-        bind-key -n C-k  if-shell  "$is_vim"  "send-keys C-k"  "select-pane -U"
-        bind-key -n C-l   if-shell  "$is_vim"  "send-keys C-l"   "select-pane -R"
-        bind-key -n C-\   if-shell  "$is_vim"  "send-keys C-\\"  "select-pane -l"
-
-        set -g base-index 1
         setw -g mouse on
         '';
   };

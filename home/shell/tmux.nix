36fd98fc4156f82efeb6120e384c73bb33fcc8f2
jimmy-sama <lungmuss@proton.me>
2023-11-08 20:52:23 +0100

    catppuccin options don't work in tmux

--- home/shell/tmux.nix
+++ home/shell/tmux.nix
@@ -5,21 +5,7 @@
     enable = true;
     plugins = with pkgs.tmuxPlugins; [
             sensible
-            {
-                plugin = catppuccin; 
-                extraConfig = ''
-                    set -g @catppuccin_window_right_separator "█ "
-                    set -g @catppuccin_window_number_position "right"
-                    set -g @catppuccin_window_middle_separator " | "
-                    set -g @catppuccin_window_default_fill "none"
-                    set -g @catppuccin_window_current_fill "all"
-                    set -g @catppuccin_status_modules_right "session host date_time"
-                    set -g @catppuccin_status_left_separator "█"
-                    set -g @catppuccin_status_right_separator "█"
-                    set -g @catppuccin_date_time_text "%m-%d %H:%M"
-                    set -g @catppuccin_flavour 'macchiato'
-                '';
-            }
+            catppuccin
         ];
     baseIndex = 1;
     prefix = "C-a";
